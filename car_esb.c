991b599a33e7174237cf9cbf8abc5e59de75132b
cody <sebastian.kotulla@gmx.de>
2016-05-14 19:19:23 +0200

    Works now.

--- car_esb.c
+++ car_esb.c
@@ -68,16 +68,17 @@ uint32_t InitEsb()
 {
     uint32_t err_code;
     uint8_t base_addr_0[4] = {0xE7, 0xE7, 0xE7, 0xE7};
-    uint8_t base_addr_1[4] = {0xC2, 0xC2, 0xC2, 0xC2};
-    uint8_t addr_prefix[8] = {0xE7, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8 };
+    uint8_t base_addr_1[4] = {0xE7, 0xE7, 0xE7, 0xE7};
+    uint8_t addr_prefix[8] = {0xE7, 0xE7, 0xE7, 0xE7, 0xE7, 0xE7, 0xE7, 0xE7 };
 
     nrf_esb_config_t nrf_esb_config         = NRF_ESB_DEFAULT_CONFIG;
     nrf_esb_config.protocol                 = NRF_ESB_PROTOCOL_ESB_DPL;
     nrf_esb_config.retransmit_delay         = 600;
-    nrf_esb_config.bitrate                  = NRF_ESB_BITRATE_2MBPS;
+    nrf_esb_config.bitrate                  = NRF_ESB_BITRATE_1MBPS;
     nrf_esb_config.event_handler            = nrf_esb_event_handler;
-    nrf_esb_config.mode                     = NRF_ESB_MODE_PTX;
+    nrf_esb_config.mode                     = NRF_ESB_MODE_PRX;
     nrf_esb_config.selective_auto_ack       = false;
+    nrf_esb_config.crc                      = NRF_ESB_CRC_8BIT;
 
     err_code = nrf_esb_init(&nrf_esb_config);
 
