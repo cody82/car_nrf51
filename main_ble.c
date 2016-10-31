3589c6abb1bedc3d7fd11fe6635ab0a6559c4ff6
cody <sebastian.kotulla@gmx.de>
2016-10-31 02:24:16 +0100

    Advertise forever.

--- main_ble.c
+++ main_ble.c
@@ -47,7 +47,7 @@
 #define DEVICE_NAME                      "Car"                               /**< Name of device. Will be included in the advertising data. */
 #define MANUFACTURER_NAME                "Cody"                      /**< Manufacturer. Will be passed to Device Information Service. */
 #define APP_ADV_INTERVAL                 300                                        /**< The advertising interval (in units of 0.625 ms. This value corresponds to 25 ms). */
-#define APP_ADV_TIMEOUT_IN_SECONDS       180                                        /**< The advertising timeout in units of seconds. */
+#define APP_ADV_TIMEOUT_IN_SECONDS       0                                        /**< The advertising timeout in units of seconds. */
 
 #define APP_TIMER_PRESCALER              0                                          /**< Value of the RTC1 PRESCALER register. */
 #define APP_TIMER_OP_QUEUE_SIZE          4                                          /**< Size of timer operation queues. */
