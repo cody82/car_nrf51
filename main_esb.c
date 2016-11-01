38cd8fd2e74d260270df7e6c88d57df5c7575943
cody <sebastian.kotulla@gmx.de>
2016-11-01 23:41:14 +0100

    Fix ESB loop timing.

--- main_esb.c
+++ main_esb.c
@@ -18,6 +18,45 @@
 #include "led.h"
 #include "car.h"
 
+#include "app_timer.h"
+#include "app_util_platform.h"
+
+#define APP_TIMER_PRESCALER              15                                          /**< Value of the RTC1 PRESCALER register. */
+#define APP_TIMER_OP_QUEUE_SIZE          4                                          /**< Size of timer operation queues. */
+#define TIMER_INTERVAL         APP_TIMER_TICKS(20, APP_TIMER_PRESCALER)
+
+APP_TIMER_DEF(m_app_timer_id);
+
+static volatile uint32_t timer_tick = 0;
+
+static void timer_timeout_handler(void * p_context)
+{
+    UNUSED_PARAMETER(p_context);
+    timer_tick++;
+}
+
+static void timers_init(void)
+{
+    // Initialize timer module.
+    APP_TIMER_INIT(APP_TIMER_PRESCALER, APP_TIMER_OP_QUEUE_SIZE, false);
+
+    // Create timers.
+
+    /* YOUR_JOB: Create any timers to be used by the application.
+                 Below is an example of how to create a timer.
+                 For every new timer needed, increase the value of the macro APP_TIMER_MAX_TIMERS by
+                 one.*/
+    uint32_t err_code;
+    err_code = app_timer_create(&m_app_timer_id, APP_TIMER_MODE_REPEATED, timer_timeout_handler);
+    APP_ERROR_CHECK(err_code);
+}
+static void application_timers_start(void)
+{
+    /* YOUR_JOB: Start your timers. below is an example of how to start a timer.*/
+    uint32_t err_code;
+    err_code = app_timer_start(m_app_timer_id, TIMER_INTERVAL, NULL);
+    APP_ERROR_CHECK(err_code);
+}
 /*
 const nrf_drv_rtc_t rtc =
 {
@@ -26,12 +65,29 @@ const nrf_drv_rtc_t rtc =
  .instance_id = 1
 };
 */
+static void lfclk_start_no_softdevice(void)
+{
+    nrf_clock_event_clear(NRF_CLOCK_EVENT_LFCLKSTARTED);
+    nrf_clock_int_enable(NRF_CLOCK_INT_LF_STARTED_MASK);
+    nrf_clock_task_trigger(NRF_CLOCK_TASK_LFCLKSTART);
+}
+
+ret_code_t nrf_drv_clock_init_no_softdevice(void)
+{
+    nrf_clock_xtalfreq_set(CLOCK_CONFIG_XTAL_FREQ);
+    nrf_clock_lf_src_set((nrf_clock_lfclk_t)CLOCK_CONFIG_LF_SRC);
+    //nrf_drv_common_irq_enable(POWER_CLOCK_IRQn, CLOCK_CONFIG_IRQ_PRIORITY);
+
+	return NRF_SUCCESS;
+}
+
+
 static void lfclk_config(void)
 {
-	ret_code_t err_code = nrf_drv_clock_init();
+	ret_code_t err_code = nrf_drv_clock_init_no_softdevice();
 	APP_ERROR_CHECK(err_code);
 
-	nrf_drv_clock_lfclk_request(NULL);
+	lfclk_start_no_softdevice();
 }
 
 void clocks_start( void )
@@ -41,6 +97,7 @@ void clocks_start( void )
 
     while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0);
 }
+
 /*
 static void rtc_handler(nrf_drv_rtc_int_type_t int_type)
 {
@@ -124,22 +181,26 @@ void loop()
 		BeepOff();
 	}
 
-	if ((tick % 3) == 0)
+	//if ((tick % 3) == 0)
 	{
+    	__disable_irq();
 		SetServo(RemoteFail() ? 0 : packet.steering);
+		__enable_irq();
 	}
 
 	BlinkerTick();
 
-	nrf_delay_ms(10);
+	//nrf_delay_ms(10);
 	tick++;
 	Led2Toggle();
 }
 
+static volatile uint32_t loop_tick = 0;
 void main_esb()
 {
 	clocks_start();
 	lfclk_config();
+	timers_init();
 
 //	rtc_config();
 
@@ -147,8 +208,14 @@ void main_esb()
 
 	InitEsb();
 
+	application_timers_start();
+
 	while (1)
 	{
-		loop();
+		if(loop_tick != timer_tick)
+		{
+			loop_tick = timer_tick;
+			loop();
+		}
 	}
 }
